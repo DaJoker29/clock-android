1bbfd2ca1deb1faeefd99361e20e5c728700b0af
Dewitt A. Buckingham, III <dj729@comcast.net>
2018-11-01 00:30:59 -0700

    Created a new activity to create a project.

--- app/src/main/java/com/zerodaedalus/www/clock/MainActivity.java
+++ app/src/main/java/com/zerodaedalus/www/clock/MainActivity.java
@@ -8,11 +8,20 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.zerodaedalus.www.clock.MESSAGE";
+    public static final String EXTRA_NEW_PROJECT = "com.zerodaedalus.www.clock.NEW_PROJECT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createProject(View view) {
+        Intent intent = new Intent(this, CreateProjectActivity.class);
+        EditText newProject = (EditText) findViewById(R.id.editText);
+        String projectName = newProject.getText().toString();
+        intent.putExtra(EXTRA_NEW_PROJECT, projectName);
+        startActivity(intent);
     }
 
     public void sendMessage(View view) {
