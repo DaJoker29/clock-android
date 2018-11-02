4e93d099dd17eba9b4f2096ef555c6e69472185e
Dewitt A. Buckingham, III <dj729@comcast.net>
2018-11-01 21:55:52 -0700

    Display project name and clean up old files.

--- app/src/main/java/com/zerodaedalus/www/clock/MainActivity.java
+++ app/src/main/java/com/zerodaedalus/www/clock/MainActivity.java
@@ -1,34 +1,49 @@
 package com.zerodaedalus.www.clock;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.zerodaedalus.www.clock.MESSAGE";
-    public static final String EXTRA_NEW_PROJECT = "com.zerodaedalus.www.clock.NEW_PROJECT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateCurrentProject();
     }
 
-    public void createProject(View view) {
-        Intent intent = new Intent(this, CreateProjectActivity.class);
-        EditText newProject = (EditText) findViewById(R.id.editText);
-        String projectName = newProject.getText().toString();
-        intent.putExtra(EXTRA_NEW_PROJECT, projectName);
-        startActivity(intent);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateCurrentProject();
     }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public void updateCurrentProject() {
+        // Fetch current project from Shared Preferences and display
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        String currentProject = sharedPref.getString("current_project", "No active project");
+
+        // Write new name to text view
+        TextView textView = findViewById(R.id.currentProjectDisplay);
+        textView.setText(currentProject);
+    }
+
+    public void setProject(View view) {
+        // Fetch input text
+        EditText newProjectName = (EditText) findViewById(R.id.newProjectName);
+        String currentProject = newProjectName.getText().toString();
+
+        // Save input text as current project name
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("current_project", currentProject);
+        editor.commit();
+        recreate();
     }
 }
